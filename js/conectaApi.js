74a9380c69c8e4fdc6e8f97e0f229c54232153c4
fmarga <marga.fmartins@gmail.com>
2023-01-05 15:50:13 -0300

    modulo 3

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,25 @@ async function listaVideos() {
   return connConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conn = await fetch("http://localhost:300/videos", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    }),
+  });
+
+  const connConvertida = conn.json();
+  return connConvertida;
+}
+
 export const conectaApi = {
   listaVideos,
+  criaVideo,
 };
